a4f22597144eb2d2123a796a046ff84d95426565
MbonuJennifer <mbonuifeomajennifer@gmail.com>
2022-04-29 14:09:39 +0100

    Styled the category section

--- app.js
+++ app.js
@@ -1,24 +1,68 @@
+let products = [
+  {
+    name: "Carrot Oil",
+    tag: "Carrot Oil",
+    price: 3500,
+    inCart: 0,
+  },
+  {
+    name: "Lip Balm",
+    tag: "Lip Balm",
+    price: 3500,
+    inCart: 0,
+  },
+  {
+    name: "Serum",
+    tag: "Serum",
+    price: 3500,
+    inCart: 0,
+  },
+  {
+    name: "Mosturizing Lotion",
+    tag: "Mosturizing Lotion",
+    price: 5500,
+    inCart: 0,
+  },
+];
+
 let shoppingCart = document.querySelectorAll(".add-cart");
 for (let i = 0; i < shoppingCart.length; i++) {
   shoppingCart[i].addEventListener("click", function () {
-    cartNumbers();
+    cartNumbers(products[i]);
   });
 }
-function cartNumbers() {
+function cartNumbers(product) {
+  // console.log("The product clicked is", product);
   let productNumbers = localStorage.getItem("cartNumbers");
   productNumbers = parseInt(productNumbers);
   if (productNumbers) {
     localStorage.setItem("cartNumbers", productNumbers + 1);
-    document.querySelector(".cart-fill").textContent = productNumbers + 1;
+    document.querySelector(".cart-fill-badge").textContent = productNumbers + 1;
   } else {
     localStorage.setItem("cartNumbers", 1);
-    document.querySelector(".cart-fill").textContent = 1;
+    document.querySelector(".cart-fill-badge").textContent = 1;
   }
+  setItems(product);
+}
+function setItems(product) {
+  // console.log("inside of setItems function");
+  // console.log("My product is", product);
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+  console.log("my cartItems are", cartItems);
+  if(cartItems != null){
+    cartItems[product.tag].inCart += 1;
+  }
+  product.inCart = 1;
+  cartItems = {
+    [product.tag]: product
+  }
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
 }
 function onloadCartNumbers() {
   let productNumbers = localStorage.getItem("cartNumbers");
   if (productNumbers) {
-    document.querySelector(".cart-fill").textContent = productNumbers;
+    document.querySelector(".cart-fill-badge").textContent = productNumbers;
   }
 }
 onloadCartNumbers();
